ef11104587ba760c9a7f88ece22a970b49017231
memile123 <40469308+memile123@users.noreply.github.com>
2019-05-20 15:58:59 -0400

    javaUninstall.sh

---
+++ javaUninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -fr /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin 
+rm -fr /Library/PreferencePanes/JavaControlPanel.prefPane 
+rm -fr ~/Library/Application\ Support/Oracle/Java
