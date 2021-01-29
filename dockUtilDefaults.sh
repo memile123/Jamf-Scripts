18415b0b86c9271be40824e7b9bbfc3e08670903
memile123 <40469308+memile123@users.noreply.github.com>
2021-01-29 10:37:16 -0500

    Update dockUtilDefaults.sh

--- dockUtilDefaults.sh
+++ dockUtilDefaults.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
+### Sets Standard Dock ###
 
-DOCKUTIL=/usr/local/bin/dockutil
 
-# remove default apps
-$DOCKUTIL --remove all --no-restart
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                   V A R I A B L E S                                       #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# add items to dock
-$DOCKUTIL --add	'/Applications/Launchpad.app' --position 1 --no-restart
-$DOCKUTIL --add '/Applications/System Preferences.app' --position 2 --no-restart
-$DOCKUTIL --add '/Applications/Google Chrome.app' --position 3 --no-restart
-$DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart --allhomes
-$DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart --allhomes
-$DOCKUTIL --add '/Applications/Microsoft Powerpoint.app' --no-restart --allhomes
-$DOCKUTIL --add '/Applications/Microsoft Outlook.app' --no-restart --allhomes
-$DOCKUTIL --add '/Applications/Microsoft Teams.app' --no-restart --allhomes
-$DOCKUTIL --add '/Applications/OneDrive.app' --no-restart --allhomes
-$DOCKUTIL --add "~/Downloads" --view list --display folder --sort name --no-restart --allhomes
+dock_util=/usr/local/bin/dockutil
+fullOSVersion=$(/usr/bin/sw_vers -productVersion)
+majorOSVersion=$(/usr/bin/sw_vers -productVersion | awk -F. {'print $2'})
+minorOSVersion=$(/usr/bin/sw_vers -productVersion | awk -F. {'print $3'})
 
-#delete the job
-rm /Library/LaunchDaemons/com.dockutil.default.plist
-# unload the daemon
-launchctl remove -w /Library/LaunchDaemons/com.dockutil.default.plist
+### Checking for logged in user, assigning to the variable loggedInUser.
+if [ "$3" != "" ] && [ "$loggedInUser" == "" ]; then
+    loggedInUser=$3
+else
+    loggedInUser=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
+fi
+echo "Creating $loggedInUser's dock."
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                 A P P L I C A T I O N                                     #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+### Setting Persistent Dock Items ###
+sudo -u $loggedInUser $dock_util --remove all --no-restart '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Self Service.app' --no-restart '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Google Chrome.app' --no-restart  '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Safari.app' --no-restart  '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Microsoft Outlook.app' --no-restart '/Users/'"$loggedInUser" 
+sudo -u $loggedInUser $dock_util --add '/Applications/Microsoft Word.app' --no-restart '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Microsoft Excel.app' --no-restart '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Microsoft PowerPoint.app' --no-restart '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Microsoft OneNote.app' --no-restart  '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Skype for Business.app' --no-restart  '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Applications/Cisco/Cisco AnyConnect Secure Mobility Client.app' --no-restart '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Users/'"$loggedInUser"'/Downloads' '/Users/'"$loggedInUser"
+sudo -u $loggedInUser $dock_util --add '/Users/'"$loggedInUser"'/Documents' '/Users/'"$loggedInUser"
+
+sudo -u $loggedInUser defaults write com.apple.dock show-recents -bool FALSE
+
+
+killall -KILL Dock
+
+### Exiting gracefully ###
+exit 0
